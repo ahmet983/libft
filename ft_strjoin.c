c5e40f1b2b0f882d707d58b423235f56500b39eb
Ahmet furkan Comak <acomak@k2m04s06.42kocaeli.com.tr>
2022-02-14 14:16:32 +0300

    makefile

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: acomak <acomak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 10:21:09 by acomak            #+#    #+#             */
-/*   Updated: 2022/02/10 10:21:10 by acomak           ###   ########.fr       */
+/*   Updated: 2022/02/14 13:46:54 by acomak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,28 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	index;
-	size_t	j;
-	char	*s3;
+	unsigned int	i;
+	unsigned int	j;
+	char			*s3;
 
-	index = 0;
-	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
 	s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!s3)
 		return (NULL);
-	while (s1[index])
+	i = 0;
+	while (s1[i] != 0)
 	{
-		s3[index] = s1[index];
-		index++;
+		s3[i] = s1[i];
+		i++;
 	}
-	while (s2[j])
+	j = 0;
+	while (s2[j] != 0)
 	{
-		s3[index++] = s2[j];
+		s3[i + j] = s2[j];
 		j++;
 	}
-	s3[index] = '\0';
+	s3[i + j] = 0;
 	return (s3);
 }
+//s1 ve s2 yi birlestirir yeni bir string olusturur bunu return eder.
